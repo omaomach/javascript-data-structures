c43faa93658c7a58e9d0bf0d7c7436ff0714ba73
omaomach <aaronjoash44@gmail.com>
2023-06-06 14:55:06 +0300

    worked on remove()

--- LinkedListConstructor.js
+++ LinkedListConstructor.js
@@ -29,9 +29,9 @@ class LinkedList {
         if (!this.head) return undefined;
         let temp = this.head;
         let pre = this.head;
-        while(temp.next) {
+        while(temp.next) {   // if temp.next is pointing to another node
             pre = temp;
-            temp = temp.next;
+            temp = temp.next; // this will move the temp variable over to the next node
         }
         this.tail = pre;
         this.tail.next = null;
@@ -42,11 +42,73 @@ class LinkedList {
         }
         return temp; 
     }
+    unshift(value) {
+        const newNode = new Node(value)
+        if (!this.head) {
+            this.head = newNode
+            this.tail = newNode
+        }else {
+            newNode.next = this.head
+            this.head = newNode
+        }
+        this.length++
+        return this
+    }
+    shift() {
+        if (!this.head) return undefined
+        let temp = this.head
+        this.head = this.head.next
+        temp.next = null
+        this.length--
+        if (this.length === 0) {
+            this.tail = null
+        }
+        return temp
+    }
+    get(index) {
+        if (index < 0 || index >= this.length) return undefined
+        let temp = this.head
+        for (let i = 0; i < index; i++) {
+            temp = temp.next
+        }
+        return temp
+    }
+    set(index, value) {
+        let temp = this.get(index)
+        if (temp) {
+            temp.value = value
+            return true  // this will make the code to stop running too
+        }
+        return false
+    }
+    insert(index, value) {
+        if (index === 0) return this.unshift(value)
+        if (index === this.length) return this.push(value)
+        if (index < 0 || index >= this.length) return undefined
+        const newNode = new Node(value)
+        const temp = this.get(index - 1)
+        newNode.next = temp.next
+        temp.next = newNode
+        this.length++
+        return true
+    }
+    remove(index) {
+        if (index < 0 || index >= this.length) return undefined
+        if (index === 0) return this.shift()
+        if (index === this.length - 1) return this.pop()
+
+        let pre = this.get(index - 1)
+        let temp = pre.next  // O(1) operation
+        pre.next = temp.next
+        temp.next = null
+        this.length--
+        return temp
+    }
 }
 
 let linkedList = new LinkedList(5)
 linkedList.push(4)
-linkedList.pop()
-linkedList.pop()
-linkedList.pop()
-console.log(linkedList)
+linkedList.unshift(1)
+linkedList.insert(1,6)
+linkedList.insert(4,7)
+console.log(linkedList.remove(4))
